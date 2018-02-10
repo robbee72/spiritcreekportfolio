f8a3108dd802e296f63471232aa8b137b8a6356a
KEITH L ROBINSON <krobnson@gmail.com>
2018-02-10 03:43:36 -0600

    integrated destroy for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -34,9 +34,22 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
 
 end
