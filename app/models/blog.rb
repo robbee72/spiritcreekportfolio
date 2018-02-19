5bcfde099a93e9e966b6ac879673d64bdffa13e6
KEITH L ROBINSON <krobnson@gmail.com>
2018-02-19 13:12:00 -0600

    Updated source tracking feature

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,7 +7,7 @@ class Blog < ApplicationRecord
 
   belongs_to :topic, optional: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   scope :desc, -> {
     order("blogs DESC") }
